cbed7daf9e7c010871f8412cd2f4beb9284cd0fe
Marchelina Anjani S <marselinaanjanisaputri23@gmail.com>
2023-09-27 13:24:23 +0700

    tutor4

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from main.views import show_main, create_product, show_xml, show_json, show_xml_
 from main.views import register
 from main.views import login_user #sesuaikan dengan nama fungsi yang dibuat
 from main.views import logout_user
+from main.views import edit_product, delete_product
 
 app_name = 'main'
 
@@ -16,4 +17,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('edit-product/<int:id>', edit_product, name='edit_product'),
+    path('delete/<int:id>', delete_product, name='delete_product'),
 ]
